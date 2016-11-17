26c894fb82b3e54395f404bc614f755ac105e5a4
vpeters2 <vpeters2@bf71baf5-a347-40dc-ab8a-56b97dd86a93>
2016-11-17 20:10:56 +0000

    added email stuff

--- config.py
+++ config.py
@@ -8,4 +8,13 @@ WTF_CSRF_SECRET_KEY = 'z9y8x7w6v5u4t3s2r1'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'high5_app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
+# email server
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'high5.vpeters2'
+MAIL_PASSWORD = 'high5CS242'
 
+# administrator list
+ADMINS = ['high5.vpeters2@gmail.com']
